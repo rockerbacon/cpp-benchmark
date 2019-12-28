a62488d79322e704110fbe971af69ca746bccea5
rockerbacon <vas.vitor.santos@gmail.com>
2019-12-28 12:10:18 -0300

    Added observer tests to benchmark

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -1,6 +1,8 @@
 #include <assertions-test/test.h>
 #include <benchmark.h>
 
+#include "observer_mock.h"
+
 begin_tests {
 	test_suite("when executing benchmarks") {
 		test_case("benchmark should execute the correct number of runs") {
@@ -9,8 +11,93 @@ begin_tests {
 			benchmark ("test number of runs", number_of_runs) {
 				runs++;
 			}
-			std::cerr << "RUNS: " << runs << std::endl;
 			assert(runs, ==, number_of_runs);
 		};
 	}
+
+	test_suite("when observing variables in a single benchmark") {
+		test_case("benchmark information should be correctly reported to all observers") {
+			observer_mock observer1;
+			observer_mock observer2;
+
+			register_observer(&observer1);
+			register_observer(&observer2);
+
+			std::string benchmark_title("test run begun");
+			unsigned number_of_runs = 3;
+
+			benchmark(benchmark_title, number_of_runs) {}
+
+			assert(observer1.benchmark_title_reported, ==, benchmark_title);
+			assert(observer2.benchmark_title_reported, ==, benchmark_title);
+
+			assert(observer1.number_of_runs_reported, ==, number_of_runs);
+			assert(observer2.number_of_runs_reported, ==, number_of_runs);
+		};
+
+		test_case("benchmark begin should be correctly reported to all observers") {
+			observer_mock observer1;
+			observer_mock observer2;
+
+			register_observer(&observer1);
+			register_observer(&observer2);
+
+			std::string benchmark_title("test run begun");
+			unsigned number_of_runs = 3;
+
+			benchmark(benchmark_title, number_of_runs) {}
+
+			assert(observer1.benchmark_begun_notifications, ==, 1);
+			assert(observer2.benchmark_begun_notifications, ==, 1);
+		};
+
+		test_case("benchmark end should be correctly reported to all observers") {
+			observer_mock observer1;
+			observer_mock observer2;
+
+			register_observer(&observer1);
+			register_observer(&observer2);
+
+			std::string benchmark_title("test run begun");
+			unsigned number_of_runs = 3;
+
+			benchmark(benchmark_title, number_of_runs) {}
+
+			assert(observer1.benchmark_ended_notifications, ==, 1);
+			assert(observer2.benchmark_ended_notifications, ==, 1);
+		};
+
+
+		test_case("benchmark run begin should be correctly reported to all observers") {
+			observer_mock observer1;
+			observer_mock observer2;
+
+			register_observer(&observer1);
+			register_observer(&observer2);
+
+			std::string benchmark_title("test run begun");
+			unsigned number_of_runs = 3;
+
+			benchmark(benchmark_title, number_of_runs) {}
+
+			assert(observer1.run_begun_notifications, ==, number_of_runs);
+			assert(observer2.run_begun_notifications, ==, number_of_runs);
+		};
+
+		test_case("benchmark run end should be correctly reported to all observers") {
+			observer_mock observer1;
+			observer_mock observer2;
+
+			register_observer(&observer1);
+			register_observer(&observer2);
+
+			std::string benchmark_title("test run begun");
+			unsigned number_of_runs = 3;
+
+			benchmark(benchmark_title, number_of_runs) {}
+
+			assert(observer1.run_ended_notifications, ==, number_of_runs);
+			assert(observer2.run_ended_notifications, ==, number_of_runs);
+		};
+	};
 } end_tests;
---
+++ tests/observer_mock.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <observer.h>
+
+class observer_mock : public benchmark::Observer {
+	public:
+		std::string benchmark_title_reported;
+		unsigned number_of_runs_reported;
+		unsigned benchmark_begun_notifications;
+		unsigned run_begun_notifications;
+		unsigned run_ended_notifications;
+		unsigned benchmark_ended_notifications;
+
+		observer_mock() :
+			number_of_runs_reported(0),
+			benchmark_begun_notifications(0),
+			run_begun_notifications(0),
+			run_ended_notifications(0),
+			benchmark_ended_notifications(0)
+		{}
+
+		void notifyBenchmarkBegun(const std::string& benchmarkTitle, unsigned numberOfRuns) {
+			benchmark_title_reported = benchmarkTitle;
+			number_of_runs_reported = numberOfRuns;
+			benchmark_begun_notifications++;
+		}
+
+		void notifyRunBegun() {
+			run_begun_notifications++;
+		}
+
+		void notifyRunEnded() {
+			run_ended_notifications++;
+		}
+
+		void notifyBenchmarkEnded() {
+			benchmark_ended_notifications++;
+		}
+};
+
